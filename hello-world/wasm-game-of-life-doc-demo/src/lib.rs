52dc135321f3f59757b736a4436525deb3af2c9d
Jon Gucciardi <jon.gucciardi@catapultsystems.com>
2020-03-10 10:39:51 -0500

    logging ticks and values to console

--- hello-world/wasm-game-of-life-doc-demo/src/lib.rs
+++ hello-world/wasm-game-of-life-doc-demo/src/lib.rs
@@ -3,6 +3,7 @@ extern crate js_sys;
 extern crate web_sys;
 use std::fmt;
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -10,6 +11,23 @@ use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
+}
 
 #[wasm_bindgen]
 #[repr(u8)]
@@ -51,6 +69,7 @@ impl Cell {
 #[wasm_bindgen]
 impl Universe {
     pub fn tick(&mut self) {
+        let _timer = Timer::new("Universe::tick");
         let mut next = self.cells.clone();
 
         for row in 0..self.height {
