668efc8f40d8063bcc7051d0d5f7ad15d64bc982
Jon Gucciardi <jon.gucciardi@catapultsystems.com>
2020-03-10 09:38:02 -0500

    added play pause control

--- hello-world/wasm-game-of-life-doc-demo/src/lib.rs
+++ hello-world/wasm-game-of-life-doc-demo/src/lib.rs
@@ -1,9 +1,15 @@
 mod utils;
 extern crate js_sys;
+extern crate web_sys;
 use std::fmt;
 use wasm_bindgen::prelude::*;
 use fixedbitset::FixedBitSet;
 
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -92,6 +98,7 @@ impl Universe {
     }
 
     pub fn new() -> Universe {
+        utils::set_panic_hook();
         let width = 64;
         let height = 64;
 
@@ -126,5 +133,30 @@ impl Universe {
 
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
